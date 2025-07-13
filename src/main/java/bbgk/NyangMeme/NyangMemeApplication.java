584ae5e8f3dc54ce2b36c4c60766713ec0ed337c
ppusda <ppusda1234@gmail.com>
2025-07-13 21:56:50 +0900

    Initial commit

---
+++ src/main/java/bbgk/NyangMeme/NyangMemeApplication.java
@@ -0,0 +1,13 @@
+package bbgk.NyangMeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyangMemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyangMemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bbgk/NyangMeme/NyangMemeApplicationTests.java
@@ -0,0 +1,13 @@
+package bbgk.NyangMeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyangMemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
